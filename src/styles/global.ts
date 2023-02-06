7b6f513dfdab10bad12d83eabdf16ab4a3b823f1
joao-hideki <joaoohideki12345@gmail.com>
2023-02-06 17:00:35 -0300

    montando a tela home

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle, ThemeConsumer } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     * {
@@ -8,15 +8,16 @@ export const GlobalStyle = createGlobalStyle`
     }
     :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${({theme}) => theme['green-500']};
+        box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
     }
     body {
-        background-color: ${({theme}) => theme['gray-900']};
-        color: ${({theme}) => theme['gray-300']};
+        background-color: ${({ theme }) => theme['gray-900']};
+        color: ${({ theme }) => theme['gray-300']};
+        -webkit-font-smoothing: antialiased;
     }
     body, input, textarea, button {
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: 1rem;
     }
-`;
+`
