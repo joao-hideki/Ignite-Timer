e671d012b7ffeee488e6e674196be5234d6f878f
joao-hideki <joaoohideki12345@gmail.com>
2023-02-06 14:31:32 -0300

    setando a rota home e history. E definindo o layout das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Hello World!</div>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
